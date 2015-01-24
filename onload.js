da73b8e1976a02e04cc8a7040bf4a4f125f4bf28
Boobalan <boobalan.m@doparttime.com>
2015-01-24 17:07:36 +0530

    initial code

---
+++ onload.js
@@ -0,0 +1,7 @@
+var cssstyle="<style>"+
+"a:visited {color: white;}a {color: #AAA;}body,#hdtbSum,#fbar{color: white;background: #333;}a:link, .w, .q:active, .q:visited,.tbotu,.mblink:visited,a:visited  {color:#ccc;}.f, .f a:link, .m, .c h2, #mbEnd h2, #tads h2, #tadsb h2, #tadsto h2,.descbox{color:#999;}.a,cite, cite a:link, cite a:visited, .cite, .cite:link, #mbEnd cite b, #tads cite b, #tadsb cite b, #tadsto cite b, #ans > i, .bc a:link {color:#fff;}.s {color: #888;}.s em {color: white;}#fll a, #bfl a {color:#AAA;}a.fl:link, .fl a, .flt, a.flt, .gl a:link, a.mblink, .mblink b {color: #AAA;}body, #leftnav, #tbdi, #hidden_modes, #hmp {background: #333;}.mitem > .kl, #ms > .kl {color:white;}.mitem > .kl:hover, #ms > .kl:hover{color: white;background: #555;}#lc a, .tbou > a.q, #tbpi, #tbtro, .tbt label, #prc_opt, #set_location_section a, .tbtctlabel, #swr a {color: #AAA;}"+
+"#appbar{background: #444;}#gbq1,  #gbx1,.gb_ib,.gb_Fb,.fbar,.gb_nb, .gb_Vb {background: #000;border:none;color:#fff;}.ab_wrp {border:none;}.mitem .kl:hover, .msel .kls:hover, #showmodes:hover {opacity: 1.0;background-color: #555;}.s a em,.s a:visited em{color:#fff;p}#tads, #tadsb, #tadsto {display:none;}.kno-fh ,.kno-fh a,.gb_Za{background:#aaa;color:#eee;}.kno-t,{background:#fff;}"+
+"</style>";
+$(document).ready(function(){
+	$('body').append(cssstyle);
+});
